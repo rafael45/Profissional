b45f4c3d550a6dd2f40b84042a61f5c13324a0f0
rafael45 <rafael450140@gmail.com>
2025-07-12 21:21:09 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 const express = require('express');
-const { BlobServiceClient, StorageSharedKeyCredential } = require('@azure/storage-blob');
+const { DefaultAzureCredential } = require('@azure/identity');
+const { BlobServiceClient } = require('@azure/storage-blob');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// 🔐 Substitua pelas suas informações
-const accountName = 'stfunctiontesterafaelrg'; // Nome da conta de armazenamento
-const accountKey = 'dSvCXSztNsU0uIyPlJWkTCLYNlcF6B9ql2q4MIclqSlv3xbxHW5hHjL4xDkjeVMt5zLdn7EFgwTq+AStCX9zgA==';
+// 🔐 Substitua com o nome da sua conta e container
+const accountName = 'stfunctiontesterafaelrg'; // Nome da Storage Account
 const containerName = 'teste1'; // Nome do container
-
-// 🔧 Configurações do cliente
-const sharedKeyCredential = new StorageSharedKeyCredential(accountName, accountKey);
-const blobServiceClient = new BlobServiceClient(
-  `https://${accountName}.blob.core.windows.net`,
-  sharedKeyCredential
-);
 
 // 🌐 Rota principal
 app.get('/', async (req, res) => {
   try {
+    const credential = new DefaultAzureCredential();
+    const blobServiceClient = new BlobServiceClient(
+      `https://${accountName}.blob.core.windows.net`,
+      credential
+    );
+
     const containerClient = blobServiceClient.getContainerClient(containerName);
     let list = '';
 
